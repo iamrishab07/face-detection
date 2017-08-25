26ebd84b2a97b3a170059158768f7ffd7b9c7267
iamrishab07 <smrishabnxt001@gmail.com>
2017-08-25 23:29:28 +0530

    adds dataSetTrainer file to train the program

---
+++ dataSetCreator.py
@@ -0,0 +1,27 @@
+# import the libraries
+import cv2
+import numpy as np
+
+
+facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+id = raw_input()
+sampleNum = 0
+
+while True:
+    ret,img = cap.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = facedetect.detectMultiScale(gray,1.3,5)
+
+    for(x,y,w,h) in faces:
+        sampleNum = sampleNum + 1
+        cv2.imwrite("dataSet/User." + str(id) + "." + str(sampleNum) + ".jpg",gray[y:y+h,x:x+w])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.waitKey(100)
+    cv2.imshow('Face',img)
+    if(sampleNum>20):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
